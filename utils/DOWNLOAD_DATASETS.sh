85c4294c848abf6ba7ebc965df7f72b0b547ce3a
TMats <t.matsu.changmin@gmail.com>
2023-05-20 18:57:44 +0900

    Fix dataset download scripts

--- utils/DOWNLOAD_DATASETS.sh
+++ utils/DOWNLOAD_DATASETS.sh
@@ -1,4 +1,3 @@
 mkdir ../data
-wget --recursive --no-parent --no-host-directories --cut-dirs=2 --relative -A zip https://diffusion-policy.cs.columbia.edu/data/training/ -P ../data
-unzip "*.zip"
-
+wget -c --recursive --no-parent --no-host-directories --cut-dirs=2 --relative -A zip https://diffusion-policy.cs.columbia.edu/data/training/ -P ../data
+unzip "../data/*.zip" -d ../data/
